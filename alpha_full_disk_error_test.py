3176daef58f5310c525064439f07a40a3b4b2c63
Michael Matheny <michaelmathen@gmail.com>
2019-01-29 18:31:09 -0500

    new file

--- alpha_full_disk_error_test.py
+++ alpha_full_disk_error_test.py
@@ -17,10 +17,10 @@ if __name__ == "__main__":
         c = 0
         #TODO find a good value of eps where this is converged
         red, blue = full_testing.generate_disk_sets(fname, r, p, q, min_r, max_r)
-        for region, two_l_samp, sample_method, fast_disk in [("multiscale_disk_fixed", True, "even", False), ("disk", True, "even", False), ("multiscale_disk", True, "grid_direc", False), ("multiscale_disk", True, "grid_direc", True)]:
+        for region, two_l_samp, sample_method, fast_disk in [("multiscale_disk", True, "grid_direc", True), ("multiscale_disk", True, "grid_direc", False), ("multiscale_disk_fixed", True, "even", False), ("disk", True, "even", False)]:
             full_testing.testing_full_framework(red, blue,
                                                 "full_alpha_disk_error_{}_{}_{}.csv".format(c, fname, region), -1, -5, 40,
-                                                eps=.01,
+                                                eps=.005,
                                                 vparam="alpha",
                                                 region_name=region,
                                                 sample_method=sample_method,
