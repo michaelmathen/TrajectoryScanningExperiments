839a13f592356522e69b525df1069cf927d6cb42
Michael Matheny <michaelmathen@gmail.com>
2019-01-31 14:47:24 -0500

    updating flux alt

--- flux_error_alt.py
+++ flux_error_alt.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     disc = utils.disc_to_func("disc")
     #red, blue, _, _ = pyscan.plant_partial_disk(trajectories, r, p, q, eps_r, disc)
 
-    for region_name in ["halfplane", "rectangle", "disk"]:
+    for region_name in ["halfplane"]:#, "rectangle", "disk"]:
         if region_name == "disk":
             method = pyscan.plant_disk
         elif region_name == "rectangle":
@@ -31,14 +31,14 @@ if __name__ == "__main__":
         if region_name == "halfplane":
             for ham_sand in [False, True]:
                 output_file = "flux_error_{}_{}.csv".format(region_name, "ham" if ham_sand else "rand")
-                flux_testing.testing_flux_framework(output_file, red, blue, -1, -5, 80,
+                flux_testing.testing_flux_framework(output_file, red, blue, -1, -5, 20,
                                                     region_name=region_name,
                                                     two_level_sample=True,
                                                     ham_sample=ham_sand,
                                                     max_time=1000)
         else:
             output_file = "flux_error_{}_{}.csv".format(region_name, "rand")
-            flux_testing.testing_flux_framework(output_file, red, blue, -1, -5, 80,
+            flux_testing.testing_flux_framework(output_file, red, blue, -1, -5, 20,
                                               region_name=region_name,
                                               two_level_sample=True,
                                               ham_sample=False,
